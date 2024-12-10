3c05e10a713ea50e132c05300c389359c1d3c056
pedroschneider <pedro.schneider@vegascard.com.br>
2024-12-09 21:01:02 -0300

    pretty

---
+++ lua/phbs/init.lua
@@ -0,0 +1,56 @@
+require("phbs.remap")
+require("phbs.set")
+require("cloak")
+require("telescope")
+require("harpoon")
+require("trouble")
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = {"src/parser.c"},
+    branch = "main",
+  },
+  filetype = "blade"
+}
+
+-- Define o tipo de arquivo *.blade.php como blade
+vim.api.nvim_exec([[
+  augroup BladeFileTypeRelated
+    autocmd!
+    autocmd BufNewFile,BufRead *.blade.php set ft=blade
+  augroup END
+]], false)
+
